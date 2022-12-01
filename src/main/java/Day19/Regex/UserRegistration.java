c09443fc9daeae930192eefe585a53c4ad69a8e0
Srikanthnaid <ksru044gmail.com>
2022-12-01 15:38:24 +0530

    Enter a valid first name

--- src/main/java/Day19/Regex/UserRegistration.java
+++ src/main/java/Day19/Regex/UserRegistration.java
@@ -1,10 +1,35 @@
 package Day19.Regex;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
+	static Scanner s = new Scanner(System.in);
+
+	public static void nameCheck() {
+
+		// Scanner class using to get the first name from the user.
+		System.out.print("Enter your First name: ");
+		String First_Name = s.next();
+
+		// pattern using
+		Pattern p = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+
+		Matcher m = p.matcher(First_Name);
+
+		if (m.matches()) {
+			System.out.println("Valid");
+		} else {
+			System.out.println("!Not valid.  \nEnter Correctly");
+		}
+
+	}
+
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome to UserRegistration problem");
+
+		nameCheck();
 	}
 
 }

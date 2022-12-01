ccf8ac589edbba972e3e684c16ab8c7d4eb4b40f
Srikanthnaid <ksru044gmail.com>
2022-12-01 18:08:36 +0530

    password having minimum 8 characters

--- src/main/java/Day19/Regex/UserRegistration.java
+++ src/main/java/Day19/Regex/UserRegistration.java
@@ -8,28 +8,29 @@ public class UserRegistration {
 
 	static Scanner s = new Scanner(System.in);
 
-	public static void emailCheck() {
+	public static void mobileNumberCheck() {
 
 		// Scanner class using to get the first name from the user.
-		System.out.print("Enter your Last name: ");
-		String Email = s.next();
+		System.out.print("Enter your password: ");
+		String psw = s.next();
 
 		// pattern class using to set pattern
-		Pattern p = Pattern.compile("^[a-zA-Z]+[.][A-Za-z]+@[a-zA-Z]+[.]+[a-zA-Z]{2}+[.][A-Za-z]{2}+$");
-		//Matcher class
-		Matcher m = p.matcher(Email);
-
-		if (m.matches()) {
+		Pattern p = Pattern.compile("^[A/za-z0-9]{8}$");
+		// Matcher class
+		Matcher m = p.matcher(psw);
+		boolean result = m.matches();
+		if (result) {
 			System.out.println("Valid");
 		} else {
 			System.out.println("!Not valid.  \nEnter Correctly");
+
 		}
 
 	}
 
 	public static void main(String[] args) {
 
-		emailCheck();
+		mobileNumberCheck();
 	}
 
 }
